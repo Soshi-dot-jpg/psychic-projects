758d3f74e63432f304e012545bab11845e086f00
Soshi-dot-jpg <121104303+Soshi-dot-jpg@users.noreply.github.com>
2024-04-16 22:55:10 +0200

    Update wlistdownload.bat

--- list-downloader/wlistdownload.bat
+++ list-downloader/wlistdownload.bat
@@ -1,11 +1,26 @@
-echo off
-set mods = mods.txt
+set listurl=https://raw.githubusercontent.com/Soshi-dot-jpg/psychic-projects/main/list-downloader/exemplelist.txt
+:: here, put your list of files url to download
 
-for /f "tokens=*" %%a in (mods.txt) do (
+echo off
+title list downloader
+cls
+echo.
+echo Hello, this program will download files form a list in the internet,
+echo here is some info you might need:
+echo.
+echo List URL: %listurl%
+echo.
+echo.
+echo press any key to continue and process to downloading
+pause >nul
+curl %listurl% -o list.txt
+for /f "tokens=*" %%a in (list.txt) do (
+	cls
 	echo %%a
 	curl -O %%a
+	pause
 )
-
+del list.txt
 
 echo alo
 pause
